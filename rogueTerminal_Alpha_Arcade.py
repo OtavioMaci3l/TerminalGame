76c74b6fded73c93f275f1526381ab98aa2ba4a1
OtavioMaci3l <otaviomaci3lalves@gmail.com>
2024-09-10 22:06:49 -0300

    cores e derrotra
    agora adicionei cores ao jogo e quando um mob é derrotado ele realmente é derrotado e some, quando o mob é derrotado e ainda tem outro mob tava acontecendo um bug que ele aparecia com 0 de vida e quando atualizava ele sumia de vez, agora quando o mob é derrotado ele aparece como derrotado e depois na proxima ação do player ele some

--- rogueTerminal_Alpha_Arcade.py
+++ rogueTerminal_Alpha_Arcade.py
@@ -1,12 +1,25 @@
 from random import randint
 import os
+
+#cores para deixar o terminal bonitin
+
+
+#reset cor
+res='\033[0;00;00m'
+#vermelho 
+verm='\033[1;31;40m'
+#verde
+verd='\033[1;32;40m'
+#amarelho
+amar='\033[1;33;40m'
+
 #lista de Mobs, tá npc pq é muita coisa pra muda, depois eu mudo isso para listaMOBs
 listaNPCs=[]
 
 #player
 player={
     "nome": "Lotus",
-    "level":10,
+    "level":20,
     "exp":0,
     "expMax":100,
     "hp":100,
@@ -16,9 +29,9 @@ player={
 
 #menu principal
 menuIniciar=[
-    "1. Iniciar batalha",
+    f"{verd}1. Iniciar batalha{res}",
     "2. Editar personagem",
-    "3. Sair"
+    f"{verm}3. Sair{res}"
 ]
 
 #mene de pause
@@ -71,9 +84,37 @@ def gerarMobs(quant, lvl):
 #esta função servira como um TAB para o usuário
 #irá mostrar as informações dos mobs
 def mostrarMobs():
+    corVida=verd
     print("\n------------------------------------------------------------")
     for npc in listaNPCs:
-        print(f"| Nome: {npc['nome']} || Level: {npc['level']} || Dano: {npc['dano']} || HP: {npc['hp']} / {npc['hpMax']} |\n------------------------------------------------------------")
+
+        #remove o mob derrotado
+        if npc['hp']<=0:
+            listaNPCs.remove(npc)
+            os.system('cls')
+            mostrarMobs()
+            npc['hp']="DERROTADO"
+
+        #talvez resolva o bug
+        if npc['hp']=="DERROTADO":
+            npc['hp']=0
+
+        #quando a vida do mob chega na metade ela fica amarela e quando chega na metade da metade fica
+        if npc['hp']<=npc['hpMax']/2:
+            if npc['hp']<=(npc['hpMax']/2)/2:
+                corVida=verm
+            else:
+                corVida=amar
+        else:
+            corVida=verd
+
+        #vo aproveitar um bug que ta dando para dar mais uma caracteristica ao meu jogo
+        if npc['hp']==1 or npc['hp']==0:
+            npc['hp']="DERROTADO"
+            npc['hpMax']=""
+
+        #mostra os mobs
+        print(f"| Nome: {verm}{npc['nome']}{res} || Level: {npc['level']} || Dano: {npc['dano']} || HP: {corVida}{npc['hp']}{res} / {verd}{npc['hpMax']}{res} |\n------------------------------------------------------------")
 
 
 #atacar o mob
@@ -86,8 +127,16 @@ def atacarPlayer(mob):
 
 #a inteface do player
 def hud():
+    #quando a vida do player chega na metade ela fica amarela e quando chega na metade da metade fica
+    if player['hp']<=player['hpMax']/2:
+        if player['hp']<=(player['hpMax']/2)/2:
+            corVida=verm
+        else:
+            corVida=amar
+    else:
+        corVida=verd
     print("------------------------------------------------------------")
-    print(f" /-----\ \n | <*> | \n | /|\ |  Player HP: {player['hp']} / {player['hpMax']}  \n | / \ | \n \-----/")
+    print(f" /-----\ \n | <*> | \n | /|\ |  Player HP: {corVida}{player['hp']}{res} / {player['hpMax']}  \n | / \ | \n \-----/")
     print("------------------------------------------------------------\n")
 
 #batalha
@@ -125,14 +174,14 @@ def batalha():
 def win():
     hud()
     print("------------------------------------------------------------")
-    print("Parabens você derrotou a wave")
+    print(f"{verd}Parabens você derrotou a wave{res}")
     print("------------------------------------------------------------\n")
 
 #tela de derrota e scoore da wave
 def lose():
     hud()
     print("-------------------------------------------------------------------------")
-    print("Você pode ter perdido a batalha mas não guerra vá e conquiste sua vitória")
+    print(f"{verm}Você pode ter perdido a batalha mas não guerra vá e conquiste sua vitória{res}")
     print("-------------------------------------------------------------------------\n")
 
 
@@ -142,7 +191,7 @@ def inicio():
     os.system('cls')
     while True:
         print("----------------------------")
-        print("       TERMINAL-GAME     ")
+        print(f"       {verm}TERMINAL-GAME{res}     ")
         menuInicio()
         opc=input("             ")
         if opc == "1":
@@ -169,7 +218,7 @@ def inicio():
             break
         else:
             os.system('cls')
-            print(f"'{opc}' é um código invalido")
+            print(f"'{verm}{opc}{res}' é um código invalido")
             continue
 
 
